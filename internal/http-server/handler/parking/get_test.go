1dd1658322e03a07f5bbec2ea59c0906757bcf43
Egor Kochetkov <pirs2132@gmail.com>
2025-03-14 10:12:48 +0400

    feature: added parking topology (#4)
    * feature: added migration to create parking cells table
    
    * feature: added parking cells to parking model, get and post handler, added getParking handler, added parking cells support to postgresql methods
    
    * feature: added and updated test for parking cell support

--- internal/http-server/handler/parking/get_test.go
+++ internal/http-server/handler/parking/get_test.go
@@ -2,6 +2,8 @@ package parking_test
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/PIRSON21/parking/internal/config"
@@ -9,7 +11,10 @@ import (
 	"github.com/PIRSON21/parking/internal/http-server/handler/parking/mocks"
 	"github.com/PIRSON21/parking/internal/lib/logger/handlers/slogdiscard"
 	"github.com/PIRSON21/parking/internal/models"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
@@ -20,6 +25,8 @@ const (
 	envLocal = "local"
 	envDev   = "dev"
 	envProd  = "prod"
+
+	urlAllParkings = "/parking"
 )
 
 func TestAllParkingsHandler_Success(t *testing.T) {
@@ -45,7 +52,7 @@ func TestAllParkingsHandler_Success(t *testing.T) {
 		}, nil)
 
 	// Создаем HTTP-запрос
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "/", nil)
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, urlAllParkings, nil)
 	assert.NoError(t, err, "")
 
 	// Создаем ResponseRecorder (записывает ответ)
@@ -64,7 +71,7 @@ func TestAllParkingsHandler_Success(t *testing.T) {
 
 	body := rr.Body.String()
 
-	expectedJSON := `[{"id":1,"name":"1:Центр","address":"ул. Ленина, 10","height":50,"width":20},{"id":2,"name":"1:ТЦ","address":"ул. Ленина, 20","height":40,"width":18}]`
+	expectedJSON := `[{"id":1,"name":"1:Центр","address":"ул. Ленина, 10","height":50,"width":20,"url":"/parking/1"},{"id":2,"name":"1:ТЦ","address":"ул. Ленина, 20","height":40,"width":18,"url":"/parking/2"}]`
 	assert.JSONEq(t, expectedJSON, body)
 }
 
@@ -104,7 +111,7 @@ func TestAllParkingsHandler_SearchSuccess(t *testing.T) {
 
 	body := rr.Body.String()
 
-	expectedJSON := `[{"id":1,"name":"1:Центр","address":"ул. Ленина, 10","height":50,"width":20},{"id":2,"name":"1:Центр","address":"ул. Ленина, 20","height":40,"width":18}]`
+	expectedJSON := `[{"id":1,"name":"1:Центр","address":"ул. Ленина, 10","height":50,"width":20,"url":"/parking/1"},{"id":2,"name":"1:Центр","address":"ул. Ленина, 20","height":40,"width":18,"url":"/parking/2"}]`
 
 	assert.JSONEq(t, expectedJSON, body)
 }
@@ -145,7 +152,7 @@ func TestAllParkingsHandler_ErrorDev(t *testing.T) {
 		Return(nil, errors.New("error test"))
 
 	// Создаем запрос
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "/", nil)
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, urlAllParkings, nil)
 	require.NoError(t, err)
 
 	// Создаем обработчика
@@ -172,7 +179,7 @@ func TestAllParkingsHandler_ErrorProd(t *testing.T) {
 	parkingGetterMock.On("GetParkingsList", "").
 		Return(nil, errors.New("error test"))
 
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "/", nil)
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, urlAllParkings, nil)
 	require.NoError(t, err)
 
 	rr := httptest.NewRecorder()
@@ -188,4 +195,255 @@ func TestAllParkingsHandler_ErrorProd(t *testing.T) {
 	expectedBody := fmt.Sprintln("Internal Server Error")
 	assert.Equal(t, expectedBody, body)
 
+}
+
+const (
+	expectedJSONWithoutCells = `{"id":%d,"name":%q,"address":%q,"width":%d,"height":%d}`
+	expectedJSONWithCells    = `{"id":%d,"name":%q,"address":%q,"width":%d,"height":%d,"cells":%s}`
+	expectedError            = `{"error":%q}`
+
+	urlCurrentParking = "/parking/%d"
+)
+
+// TestGetParkingHandler проверяет запрос получения данных о парковке по ID
+//
+// Если изменится адрес или ответ, измените шаблон сверху
+//
+//goland:noinspection t
+func TestGetParkingHandler(t *testing.T) {
+
+	cases := []struct {
+		// Name - название теста. Нужен только для отображения
+		Name string
+		// Parking - указатель на модель. От неё все зависит - ответы, запросы и т.д.
+		// Может быть nil, тогда запрос к БД будет на id = 1, а в результате будет nil.
+		Parking *models.Parking
+		// ResponseCode - ожидаемый код ответа. Лучше подбирать через пакет http
+		ResponseCode int
+		// GetParkingError - ожидаемая ошибка от метода GetParkingByID. Может быть nil
+		GetParkingError error
+		// GetCellsError - ожидаемая ошибка от метода GetParkingCells. Может быть nil
+		GetCellsError error
+		// WithCells - ожидать JSON ответ с клетками или без
+		WithCells bool
+		// JSON - ожидать JSON ответ или нет
+		// (при серверных ошибках на проде и при 404 ответ не JSON)
+		JSON bool
+		// Environment - значение из cfg. Для проверки ответов на проде и деве
+		Environment string
+	}{
+		{
+			Name: "Success With Cells",
+			Parking: &models.Parking{
+				ID:      1,
+				Name:    "1: Центр",
+				Address: "ул. Пушкина, д. Колотушкина",
+				Width:   4,
+				Height:  4,
+				Cells: [][]models.ParkingCell{
+					{
+						"P", "P", "P", "P",
+					},
+					{
+						".", ".", ".", ".",
+					},
+					{
+						"P", "P", ".", ".",
+					},
+					{
+						"P", "O", "I", "P",
+					},
+				},
+			},
+			ResponseCode:    http.StatusOK,
+			GetParkingError: nil,
+			GetCellsError:   nil,
+			WithCells:       true,
+			JSON:            true,
+		},
+		{
+			Name: "Success without cells",
+			Parking: &models.Parking{
+				ID:      2,
+				Name:    "1: Центр",
+				Address: "ул. Пушкина, д. Колотушкина",
+				Width:   4,
+				Height:  4,
+				Cells:   nil,
+			},
+			ResponseCode:    http.StatusOK,
+			GetParkingError: nil,
+			GetCellsError:   nil,
+			WithCells:       false,
+			JSON:            true,
+		},
+		{
+			Name: "Error while getting from DB on Dev",
+			Parking: &models.Parking{
+				ID:      2,
+				Name:    "1: Центр",
+				Address: "ул. Пушкина, д. Колотушкина",
+				Width:   4,
+				Height:  4,
+				Cells:   nil,
+			},
+			ResponseCode:    http.StatusInternalServerError,
+			GetParkingError: fmt.Errorf("db: error getting from DB"),
+			GetCellsError:   nil,
+			WithCells:       false,
+			JSON:            true,
+		},
+		{
+			Name: "Error while getting from DB on Prod",
+			Parking: &models.Parking{
+				ID:      2,
+				Name:    "1: Центр",
+				Address: "ул. Пушкина, д. Колотушкина",
+				Width:   4,
+				Height:  4,
+				Cells:   nil,
+			},
+			ResponseCode:    http.StatusInternalServerError,
+			GetParkingError: fmt.Errorf("db: error getting from DB"),
+			GetCellsError:   nil,
+			WithCells:       false,
+			Environment:     envProd,
+			JSON:            false,
+		},
+		{
+			Name:            "Success not found",
+			Parking:         nil,
+			ResponseCode:    http.StatusNotFound,
+			GetParkingError: sql.ErrNoRows,
+			GetCellsError:   nil,
+			WithCells:       false,
+			JSON:            false,
+		},
+		{
+			Name: "Error while getting cells on Dev",
+			Parking: &models.Parking{
+				ID:      2,
+				Name:    "1: Центр",
+				Address: "ул. Пушкина, д. Колотушкина",
+				Width:   4,
+				Height:  4,
+				Cells:   nil,
+			},
+			ResponseCode:    http.StatusInternalServerError,
+			GetParkingError: nil,
+			GetCellsError:   fmt.Errorf("cells: error while getting cells"),
+			WithCells:       false,
+			JSON:            true,
+		},
+		{
+			Name: "Error while getting cells on Prod",
+			Parking: &models.Parking{
+				ID:      2,
+				Name:    "1: Центр",
+				Address: "ул. Пушкина, д. Колотушкина",
+				Width:   4,
+				Height:  4,
+				Cells:   nil,
+			},
+			ResponseCode:    http.StatusInternalServerError,
+			GetParkingError: nil,
+			GetCellsError:   fmt.Errorf("cells: error while getting cells"),
+			WithCells:       false,
+			Environment:     envProd,
+			JSON:            false,
+		},
+	}
+
+	t.Parallel()
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+
+			parkingGetterMock := mocks.NewParkingGetter(t)
+			if tc.Parking != nil {
+				// Если нужны данные парковки
+				parkingGetterMock.On("GetParkingByID", tc.Parking.ID).
+					Return(&models.Parking{
+						ID:      tc.Parking.ID,
+						Name:    tc.Parking.Name,
+						Address: tc.Parking.Address,
+						Width:   tc.Parking.Width,
+						Height:  tc.Parking.Height,
+					}, tc.GetParkingError).
+					Once()
+			} else {
+				// Если парковка не нужна, задает структура только для отправки запроса по ID
+				tc.Parking = &models.Parking{ID: 1}
+				parkingGetterMock.On("GetParkingByID", tc.Parking.ID).
+					Return(nil, tc.GetParkingError).
+					Once()
+			}
+
+			parkingGetterMock.On("GetParkingCells", mock.AnythingOfType("*models.Parking")).
+				Return(tc.Parking.Cells, tc.GetCellsError).
+				Maybe()
+
+			requestURL := fmt.Sprintf(urlCurrentParking, tc.Parking.ID)
+
+			req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, requestURL, nil)
+			require.NoError(t, err)
+
+			log := slogdiscard.NewDiscardLogger()
+			cfg := &config.Config{Environment: envLocal}
+			if tc.Environment != "" {
+				// Если указан специфичный Environment
+				cfg.Environment = tc.Environment
+			}
+
+			router := chi.NewRouter()
+			router.Use(middleware.URLFormat)
+			router.Get("/parking/{id}", parking.GetParkingHandler(log, parkingGetterMock, cfg))
+
+			rr := httptest.NewRecorder()
+
+			router.ServeHTTP(rr, req)
+			require.Equal(t, tc.ResponseCode, rr.Code)
+
+			body := rr.Body.String()
+
+			var expectedResponse string
+
+			if tc.GetParkingError == nil && tc.GetCellsError == nil {
+				if tc.WithCells {
+					fmt.Println(tc.Parking.Cells)
+					cells, err := json.Marshal(tc.Parking.Cells)
+					require.NoError(t, err)
+					expectedResponse = fmt.Sprintf(expectedJSONWithCells, tc.Parking.ID, tc.Parking.Name, tc.Parking.Address, tc.Parking.Width, tc.Parking.Height, cells)
+				} else {
+					expectedResponse = fmt.Sprintf(expectedJSONWithoutCells, tc.Parking.ID, tc.Parking.Name, tc.Parking.Address, tc.Parking.Width, tc.Parking.Height)
+				}
+			}
+
+			if tc.GetParkingError != nil {
+				expectedResponse = fmt.Sprintf(expectedError, tc.GetParkingError.Error())
+			}
+
+			if tc.GetCellsError != nil {
+				expectedResponse = fmt.Sprintf(expectedError, tc.GetCellsError.Error())
+			}
+
+			if tc.JSON {
+				assert.JSONEq(t, expectedResponse, body)
+
+				return
+			} else {
+				if tc.ResponseCode == http.StatusInternalServerError {
+					assert.Equal(t, "Internal Server Error\n", body)
+
+					return
+				} else if tc.ResponseCode == http.StatusNotFound {
+					assert.Equal(t, "404 page not found\n", body)
+
+					return
+				}
+			}
+
+			assert.Fail(t, "для этого кейса не предусмотрен тест")
+		})
+	}
 }
--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -7,11 +7,12 @@ import (
 	"github.com/PIRSON21/parking/internal/config"
 	"github.com/PIRSON21/parking/internal/models"
 	"log"
+	"strings"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-// Storage хранит закрытое в пакете соединение с БД
+// Storage хранит закрытое в пакете соединение с БД.
 type Storage struct {
 	db *sql.DB
 }
@@ -36,7 +37,7 @@ func MustConnectDB(cfg *config.Config) *Storage {
 	return &Storage{db}
 }
 
-// GetParkingsList обращается в БД и получает список всех парковок (+ поиск по имени)
+// GetParkingsList обращается в БД и получает список всех парковок (+ поиск по имени).
 func (s *Storage) GetParkingsList(search string) ([]*models.Parking, error) {
 	const op = "storage.postgresql.GetParkingsList"
 
@@ -79,20 +80,155 @@ func (s *Storage) GetParkingsList(search string) ([]*models.Parking, error) {
 	return resParking, nil
 }
 
+// AddParking добавляет данные о парковке в БД.
 func (s *Storage) AddParking(parking *models.Parking) error {
 	const op = "storage.postgresql.AddParking"
 
 	stmt, err := s.db.Prepare(`
 		INSERT INTO parkings (parking_name, parking_address, parking_width, parking_height)
-		VALUES ($1, $2, $3, $4);
+		VALUES ($1, $2, $3, $4)
+		RETURNING parking_id;
 	`)
 	if err != nil {
 		return fmt.Errorf("%s: error while preparing statement: %w", op, err)
 	}
 
-	_, err = stmt.Exec(parking.Name, parking.Address, parking.Width, parking.Height)
+	err = stmt.QueryRow(parking.Name, parking.Address, parking.Width, parking.Height).Scan(&parking.ID)
 	if err != nil {
 		return fmt.Errorf("%s: error while executing statement: %w", op, err)
+	}
+
+	return nil
+}
+
+// GetParkingByID получает всю информацию (что хранится в таблице парковки) о парковке из БД..
+//
+// Возвращает указатель на модель парковки или ошибку.
+func (s *Storage) GetParkingByID(parkingID int) (*models.Parking, error) {
+	const op = "storage.postgresql.GetParkingById"
+
+	stmt, err := s.db.Prepare(`
+	SELECT 
+	    parking_id, parking_name, parking_address, parking_width, parking_height
+	FROM parkings
+	WHERE parking_id = $1;
+`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: error while preparing statement: %w", op, err)
+	}
+
+	var parking models.Parking
+	if err = stmt.QueryRow(parkingID).Scan(&parking.ID, &parking.Name, &parking.Address, &parking.Width, &parking.Height); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, err
+		}
+
+		return nil, fmt.Errorf("%s: error while executing statement: %w", op, err)
+	}
+
+	fmt.Printf("%s: parking: %v\n", op, parking)
+
+	return &parking, nil
+}
+
+// GetParkingCells получает данные о клетках из базы данных и на основе строит матрицу топологии парковки
+//
+//goland:noinspection t
+func (s *Storage) GetParkingCells(parking *models.Parking) ([][]models.ParkingCell, error) {
+	op := "storage.postgresql.GetParkingCells"
+
+	width := parking.Width
+	height := parking.Height
+
+	parkingCells := make([][]models.ParkingCell, width)
+	for x := range parkingCells {
+		parkingCells[x] = make([]models.ParkingCell, height)
+		for y := range parkingCells[x] {
+			parkingCells[x][y] = "."
+		}
+	}
+
+	stmt, err := s.db.Prepare(`
+		SELECT 
+			x,y, cell_type
+		FROM parking_cell
+		WHERE parking_id = $1;
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: error while preparing statement: %w", op, err)
+	}
+
+	rows, err := stmt.Query(parking.ID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+
+		return nil, fmt.Errorf("%s: error while getting result from DB: %w", op, err)
+	}
+
+	defer rows.Close()
+
+	found := false
+
+	for rows.Next() {
+		found = true
+		var x, y int
+
+		var cellType models.ParkingCell
+
+		if err = rows.Scan(&x, &y, &cellType); err != nil {
+			return nil, fmt.Errorf("%s: error while scanning result to var: %w", op, err)
+		}
+
+		fmt.Println(cellType)
+
+		if x < 0 || x >= height || y < 0 || y >= width {
+			continue
+		}
+
+		parkingCells[y][x] = cellType
+	}
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("%s: error after scanning rows: %w", op, err)
+	}
+
+	if !found {
+		return nil, nil
+	}
+
+	return parkingCells, nil
+
+	// TODO: короче, нужно сделать тесты, вроде все готово. может комменты ещё, хз
+}
+
+// AddCellsForParking добавляет информацию о клетках топологии парковки.
+func (s *Storage) AddCellsForParking(parking *models.Parking, cells []*models.ParkingCellStruct) error {
+	const op = "storage.postgresql.AddCellsForParking"
+
+	if len(cells) == 0 {
+		return nil
+	}
+
+	valueStrings := make([]string, 0, len(cells))
+	valueArgs := make([]interface{}, 0, len(cells)*3)
+
+	for i, cell := range cells {
+		valueStrings = append(valueStrings, fmt.Sprintf("(%d, $%d, $%d, $%d)", parking.ID, i*3+1, i*3+2, i*3+3))
+		valueArgs = append(valueArgs, cell.X, cell.Y, cell.CellType)
+	}
+
+	query := fmt.Sprintf(
+		"INSERT INTO parking_cell (parking_id, x, y, cell_type) VALUES %s ON CONFLICT (parking_id, x, y) DO UPDATE SET cell_type = EXCLUDED.cell_type",
+		strings.Join(valueStrings, ", "),
+	)
+
+	fmt.Println(query)
+	fmt.Println(valueArgs)
+
+	_, err := s.db.Exec(query, valueArgs...)
+	if err != nil {
+		return fmt.Errorf("%s: error while executing query: %w", op, err)
 	}
 
 	return nil
