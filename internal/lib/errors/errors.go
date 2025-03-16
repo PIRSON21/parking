7acf91d53c9c976f3e631d6c347f0747327c655a
Egor Kochetkov <pirs2132@gmail.com>
2025-03-16 15:43:56 +0400

    refactor: added already exist error

--- internal/lib/errors/errors.go
+++ internal/lib/errors/errors.go
@@ -5,3 +5,5 @@ import "errors"
 var ErrUnauthorized = errors.New("unauthorized")
 
 var ErrSessionExpired = errors.New("session expired")
+
+var ErrManagerAlreadyExists = errors.New("такой менеджер уже существует")
